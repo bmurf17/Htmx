d05dca889fe21dfb685f74cfa21f2280e6bd8a6d
Brendan Murphy <brendan.murphy@ostusa.com>
2024-03-15 18:00:56 -0400

    SQLite set up

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github/bmurf17/htmx-go-todo
 
 go 1.22.1
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
-	"text/template"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	err := openDB()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer closeDB()
+	err = setupDB()
+	if err != nil {
+		log.Panic(err)
+	}
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
